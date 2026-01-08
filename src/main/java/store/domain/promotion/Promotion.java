ed02dd939884fcbd8af5c98655ca80ffa0cc2260
toctoce <toctoce@naver.com>
2026-01-08 23:54:15 +0900

    feat(Promotion): 특정 날짜가 프로모션 기간인지 반환

--- src/main/java/store/domain/promotion/Promotion.java
+++ src/main/java/store/domain/promotion/Promotion.java
@@ -1,10 +1,19 @@
 package store.domain.promotion;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 
 public record Promotion(String name, int buyCount, int giveCont, LocalDate start, LocalDate end) {
 
     public static Promotion of(String name, int buyCount, int giveCount, LocalDate start, LocalDate end) {
         return new Promotion(name, buyCount, giveCount, start, end);
+    }
+
+    public boolean availableToday() {
+        LocalDate now = DateTimes.now().toLocalDate();
+        if (now.compareTo(start) < 0 || now.compareTo(end) > 0) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/store/domain/promotion/Promotions.java
+++ src/main/java/store/domain/promotion/Promotions.java
@@ -1,10 +1,18 @@
 package store.domain.promotion;
 
 import java.util.List;
+import store.common.StoreException;
 
 public record Promotions(List<Promotion> promotions) {
 
     public static Promotions of(List<Promotion> promotions) {
         return new Promotions(promotions);
+    }
+
+    public Promotion findByName(String name) {
+        return promotions.stream()
+                .filter(promotion -> promotion.name().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new StoreException("[ERROR] 잘못된 프로모션 이름입니다."));
     }
 }
